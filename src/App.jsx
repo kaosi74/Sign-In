db7b5fb7f6a03b0920222b65610ba49be330956c
Kosi74 <kaosinwankwo74@gmail.com>
2024-01-21 12:43:57 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,35 @@
 import "./Re.css";
+import { useState } from "react";
 
 function App() {
+  const [nums, setNums] = useState(0);
+  const [num, setNum] = useState(0);
+  const [firstButtonTimes, setFirstButtonTimes] = useState("time")
+  const [secondButtonTimes, setSecondButtonTimes] = useState("time")
+
+  function firstButton() {
+    setNums(nums + 1)
+    if (nums >= 0) {
+      setFirstButtonTimes("times")
+    }
+  }
+
+  function secondButton() {
+    setNum(num - 1);
+    if (num <= 1) {
+      setSecondButtonTimes("times")
+    }
+  }
+
   return (
-    <>
-      <div className="container-fluid mx-2">
-        <div className="row">
-          <div className="col-md-3"></div>
-          <div className="col-12 col-md-6">
-            <div className="forms text-center px-3">
-              <h1 className="text-white mb-3">Sign Up</h1>
-              <div className="d-flex justify-content-between mb-2">
-                <input className="form-control me-1" placeholder="First Name" />
-                <input className="form-control" placeholder="Last Name" />
-              </div>
-              <input className="form-control mb-2" placeholder="Email" />
-              <input className="form-control mb-2" placeholder="Password" />
-              <input className="form-control" placeholder=" Confirm Password" />
-              <div className="mt-4">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="gridCheck"
-                />
-                <label className="form-check-label ms-1" htmlFor="gridCheck">
-                  I accept the Terms of Use & Privacy Policy
-                </label>
-              </div>
-              <button className="btn btns my-2">Sign up</button>
-              <p>
-                Already have an account? <a className="link" href="#">Login here</a>
-              </p>
-              <p>Copyright &copy; Kling.com</p>
-            </div>
-          </div>
-          <div className="col-md-3"></div>
-        </div>
+    <div>
+      <div className="btnContainer">
+        <button className="btns" onClick={firstButton}>
+          clicked {nums} {firstButtonTimes}
+        </button>
+        <button onClick={secondButton}>clicked {num} {secondButtonTimes}</button>
       </div>
-    </>
+    </div>
   );
 }
 
